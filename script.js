710d09371042d96159df83631e6ffe39813e94e5
CatL <catherinelui.2@gmail.com>
2020-08-10 14:27:49 -0600

    Fixed the movie search div showing up on front page Updated score div, so it is in a card now Updated year since released into movie-display div

--- script.js
+++ script.js
@@ -8,169 +8,165 @@ let triviaQuestionIndex = 0;
 let apiResponse = "";
 
 startButton.on("click", function () {
-  main.css("display", "none");
-  header.css("display", "none");
-  startQuiz();
+    main.css("display", "none");
+    header.css("display", "none");
+    startQuiz();
 });
 
 function startQuiz() {
-  event.preventDefault();
-  $("#main").fadeOut("slow");
-  $("#main").empty();
-  triviaCall();
+    event.preventDefault();
+    $("#main").fadeOut("slow");
+    $("#main").empty();
+    $("#questions").css("display", "block");
+    triviaCall();
 }
 
 function triviaCall() {
-  $.ajax({
-    dataType: "json",
-    url:
-      "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple",
-    method: "GET",
-  }).then(function (response) {
-    $("#main").fadeIn("slow");
-    apiResponse = response.results;
-    renderQuestions();
-    // if then statements to run through correct/ wrong answers scenarios
-  });
+    $.ajax({
+        dataType: "json",
+        url: "https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple",
+        method: "GET",
+    }).then(function (response) {
+        $("#main").fadeIn("slow");
+        apiResponse = response.results;
+        renderQuestions();
+        // if then statements to run through correct/ wrong answers scenarios
+    });
 }
 
 function renderQuestions() {
-  $("#questions").empty();
-  const triviaRes = apiResponse;
-  const trivia = triviaRes[triviaQuestionIndex];
-  let questionnaireCards = $("#questions");
-  let triviaQuestionsRes = $("<p>").addClass("card-text").html(trivia.question);
-  let correctAnswerRes = trivia.correct_answer;
-  let incorrectAnswersRes = trivia.incorrect_answers;
-  incorrectAnswersRes.push(correctAnswerRes);
-  let totalAnswers = incorrectAnswersRes;
-  // set up p tags for questions
+    $("#questions").empty();
+    const triviaRes = apiResponse;
+    const trivia = triviaRes[triviaQuestionIndex];
+    let questionnaireCards = $("#questions");
+    let triviaQuestionsRes = $("<p>").addClass("card-text").html(trivia.question);
+    let correctAnswerRes = trivia.correct_answer;
+    let incorrectAnswersRes = trivia.incorrect_answers;
+    incorrectAnswersRes.push(correctAnswerRes);
+    let totalAnswers = incorrectAnswersRes;
+    // set up p tags for questions
 
-  let displayedQuestions = $("<p>").html(triviaQuestionsRes);
+    let displayedQuestions = $("<p>").html(triviaQuestionsRes);
 
-  for (let j = 0; j < totalAnswers.length; j++) {
-    // set up buttons for choices
-    let buttonEl = $("<button>");
-    buttonEl
-      .addClass("button small large-only-expanded answer-button")
-      .html(incorrectAnswersRes[j]);
-    buttonEl.val(incorrectAnswersRes[j]);
-    // append one question and its choice within the same box
-    let questionnaireCardBody = $("<div>").addClass("card-section");
-    questionnaireCardBody.append(displayedQuestions[j]);
-    questionnaireCardBody.append(buttonEl);
-    let questionnaireCard = $("<div>").addClass("card");
-    questionnaireCard.append(questionnaireCardBody);
-    questionnaireCards.append(questionnaireCard);
-  }
+    for (let j = 0; j < totalAnswers.length; j++) {
+        // set up buttons for choices
+        let buttonEl = $("<button>");
+        buttonEl.addClass("button small large-only-expanded answer-button").html(incorrectAnswersRes[j]);
+        buttonEl.val(incorrectAnswersRes[j]);
+        // append one question and its choice within the same box
+        let questionnaireCardBody = $("<div>").addClass("card-section");
+        questionnaireCardBody.append(displayedQuestions[j]);
+        questionnaireCardBody.append(buttonEl);
+        let questionnaireCard = $("<div>").addClass("card");
+        questionnaireCard.append(questionnaireCardBody);
+        questionnaireCards.append(questionnaireCard);
+    }
 }
 
 $(document).on("click", ".answer-button", function () {
-  const correct = $(this).val();
-  if (correct === apiResponse[triviaQuestionIndex].correct_answer) {
-    score++;
-    console.log(score);
-  }
-  triviaQuestionIndex++;
-  if (triviaQuestionIndex <= 9) {
-    renderQuestions();
-  } else if ((triviaQuestionIndex) => 0) {
-    endQuiz();
-  }
+    const correct = $(this).val();
+    if (correct === apiResponse[triviaQuestionIndex].correct_answer) {
+        score++;
+        console.log(score);
+    }
+    triviaQuestionIndex++;
+    if (triviaQuestionIndex <= 9) {
+        renderQuestions();
+    } else if ((triviaQuestionIndex) => 0) {
+        endQuiz();
+    }
 
-  // empty the question div
-  //display score
-  //display our movieInput and SearchBtn style to block
-  // }
+    // empty the question div
+    //display score
+    //display our movieInput and SearchBtn style to block
+    // }
 });
 
 function endQuiz() {
-  $(".card-section").empty();
+    $(".card-section").empty();
+    $("#questions").css("display", "none");
+    $("#scores").css("display", "block");
 
-  let scoreCard = $("<div>").addClass("card score").text(score);
+    let scoreCard = $("<div>").addClass("card-section").text(score);
 
-  $("#scores").append(scoreCard);
-  scoreQuiz();
+    $("#scores").addClass("card-section").append(scoreCard);
+    scoreQuiz();
+    $("#movieSearch").css("display", "block");
 }
 
 function scoreQuiz() {
-  // const totalScore =
-  // (correct_answer / triviaQuestionIndex);
+    // const totalScore =
+    // (correct_answer / triviaQuestionIndex);
 
-  if (score >= 9) {
-    $("#scores").append(
-      "Well Done! Keep up the good work and go watch some more movies."
-    );
-  } else if (score >= 7) {
-    $("#scores").append(
-      "Your Score wasn't horrible. Improve your chances next time by checking out more movies."
-    );
-  } else if (score >= 5) {
-    $("#scores").append(
-      "Well... might I suggest incorporating some more movie nights?"
-    );
-  } else if (score >= 2) {
-    $("#scores").append(
-      "Who's keeping score anyway? Here go check out some movies."
-    );
-  } else {
-    $("#scores").append(
-      "Your feelings matter. You should treat yourself to more movies."
-    );
-  }
+    if (score >= 9) {
+        $("#scores").append("Well Done! Keep up the good work and go watch some more movies.");
+    } else if (score >= 7) {
+        $("#scores").append("Your Score wasn't horrible. Improve your chances next time by checking out more movies.");
+    } else if (score >= 5) {
+        $("#scores").append("Well... might I suggest incorporating some more movie nights?");
+    } else if (score >= 2) {
+        $("#scores").append("Who's keeping score anyway? Here go check out some movies.");
+    } else {
+        $("#scores").append("Your feelings matter. You should treat yourself to more movies.");
+    }
 }
 
 // Movie Search (AJAX CALL)
 var movies = [];
 var movieDisplay = $("movie-display");
 $("#movieSearchBtn").on("click", function (event) {
-  event.preventDefault();
-  var movieTitle = $("#movieInput").val();
+    event.preventDefault();
+    var movieTitle = $("#movieInput").val();
 
-  $.ajax({
-    url: `https://www.omdbapi.com/?t=${movieTitle}&apikey=trilogy`,
-    method: "GET",
-  }).then(function (response) {
-    // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
+    $.ajax({
+        url: `https://www.omdbapi.com/?t=${movieTitle}&apikey=trilogy`,
+        method: "GET",
+    }).then(function (response) {
+        $("#movie-display").empty();
+        // Creating a div to hold the movie
+        var movieDiv = $("<div class='movie'>");
 
-    // Storing the rating data
-    var rating = response.Rated;
+        // Storing the rating data
+        var rating = response.Rated;
 
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
+        // Creating an element to have the rating displayed
+        var pOne = $("<p>").text("Rating: " + rating);
 
-    // Displaying the rating
-    movieDiv.append(pOne);
+        // Displaying the rating
+        movieDiv.append(pOne);
 
-    // Storing the release year
-    var released = response.Released;
+        // Storing the release year
+        var released = response.Released;
 
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
+        // Creating an element to hold the release year
+        var pTwo = $("<p>").text("Released: " + released);
 
-    // Displaying the release year
-    movieDiv.append(pTwo);
+        // Years since movie was released
+        var releasedSince = $("<p>").text(`${moment().format("YYYY") - response.Year} year(s) since this movie came out!`);
+        movieDiv.append(releasedSince);
 
-    // Storing the plot
-    var plot = response.Plot;
+        // Displaying the release year
+        movieDiv.append(pTwo);
 
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
+        // Storing the plot
+        var plot = response.Plot;
 
-    // Appending the plot
-    movieDiv.append(pThree);
+        // Creating an element to hold the plot
+        var pThree = $("<p>").text("Plot: " + plot);
 
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
+        // Appending the plot
+        movieDiv.append(pThree);
 
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
+        // Retrieving the URL for the image
+        var imgURL = response.Poster;
 
-    // Appending the image
-    movieDiv.append(image);
+        // Creating an element to hold the image
+        var image = $("<img>").attr("src", imgURL);
 
-    // Putting the entire movie above the previous movies
-    $("#movie-display").prepend(movieDiv);
-  });
+        // Appending the image
+        movieDiv.append(image);
+
+        // Putting the entire movie above the previous movies
+        $("#movie-display").prepend(movieDiv);
+    });
 });
